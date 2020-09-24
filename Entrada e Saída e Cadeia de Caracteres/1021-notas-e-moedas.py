aff6f895af38fdba026d61e1014ec70f40fd16af
Abner Santana <abnermuxah@gmail.com>
2020-09-24 15:21:01 -0300

    resolvido
    ta dando um erro no final, ao inves de ser 3 moedas de 0.01 ta dando 2 moedas de 0.01

---
+++ Entrada e Saída e Cadeia de Caracteres/1021-notas-e-moedas.py
@@ -0,0 +1,21 @@
+#N =  float(input())
+N = 576.63
+#N = 4
+if (0 <= N <= 1000000.00):
+    NOTAS = [100, 50, 20, 10, 5, 2]
+    print("NOTAS: ") #TEM QUE TA IGUAL A SAIDA DO PROBLEMA
+    for i in range(len(NOTAS)):
+        qtd_nota = int(N / NOTAS[i]) # valor informado divido pelo valor do indice atual é a qtd de nota em questão
+        print(qtd_nota,"nota(s) de R$ :",NOTAS[i] )
+        # subtrarir a diferença das notas colocadas acima
+        N -= qtd_nota * NOTAS[i]
+    # o mesmo raciocinio aplica-se as muedas
+    MOEDAS = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+    print("MOEDAS: ")
+    for i in range(len(MOEDAS)):
+        qtd_nota = int(N / MOEDAS[i]) # valor informado divido pelo valor do indice atual é a qtd de nota em questão
+        print(qtd_nota,"moeda(s) de R$ :",MOEDAS[i] )
+        # subtrarir a diferença das notas colocadas acima
+        N -= qtd_nota * MOEDAS[i]
+else:
+    print("dinheiro invalido, ta dando certo nao carai, tenta de novo ai")
