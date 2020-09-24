1a234a84cf1485cfb563a4791b079b6bd12769db
Abner Santana <abnermuxah@gmail.com>
2020-09-24 16:02:46 -0300

    update

--- Entrada e Saída e Cadeia de Caracteres/1021-notas-e-moedas.py
+++ Entrada e Saída e Cadeia de Caracteres/1021-notas-e-moedas.py
@@ -1,5 +1,5 @@
-#N =  float(input())
-N = 576.63
+N =  float(input())
+#N = 576.63
 #N = 4
 if (0 <= N <= 1000000.00):
     NOTAS = [100, 50, 20, 10, 5, 2]
@@ -10,12 +10,14 @@ if (0 <= N <= 1000000.00):
         # subtrarir a diferença das notas colocadas acima
         N -= qtd_nota * NOTAS[i]
     # o mesmo raciocinio aplica-se as muedas
-    MOEDAS = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+    MOEDAS = [1, 0.5, 0.25, 0.10, 0.05, 0.01]
     print("MOEDAS: ")
+    #print("%.2f"%N)
     for i in range(len(MOEDAS)):
         qtd_nota = int(N / MOEDAS[i]) # valor informado divido pelo valor do indice atual é a qtd de nota em questão
-        print(qtd_nota,"moeda(s) de R$ :",MOEDAS[i] )
+        print(qtd_nota,"moeda(s) de R$ :", MOEDAS[i] )
         # subtrarir a diferença das notas colocadas acima
         N -= qtd_nota * MOEDAS[i]
+        N = round(N,2)
 else:
     print("dinheiro invalido, ta dando certo nao carai, tenta de novo ai")
