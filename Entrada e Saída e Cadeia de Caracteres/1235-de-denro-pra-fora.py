719cee6053cb9ce4f6f8cf0ac3037e518648eb68
Abner Santana <abnermuxah@gmail.com>
2020-09-26 17:11:48 -0300

    Update 1235-de-denro-pra-fora.py

--- Entrada e Saída e Cadeia de Caracteres/1235-de-denro-pra-fora.py
+++ Entrada e Saída e Cadeia de Caracteres/1235-de-denro-pra-fora.py
@@ -1,20 +1,21 @@
 num_linhas = int(input())
 for i in range (num_linhas):
     texto = input()
-    # dividir a linha por 2 (texto1 e texto 2)
-    div = int(len(texto)/2)
-    # salvar a primeira parte em texto 1
-    texto1 = texto[:div]
-    # inverter a string 
-    texto1 = texto1[::-1]
+    if (2 <= len(texto) <= 100):
+        # dividir a linha por 2 (texto1 e texto 2)
+        div = int(len(texto)/2)
+        # salvar a primeira parte em texto 1
+        texto1 = texto[:div]
+        # inverter a string 
+        texto1 = texto1[::-1]
 
-    # salvar a segunda parte em texto 2
-    texto2 = texto[div:]
-    # inverter a string 
-    texto2 = texto2[::-1]
+        # salvar a segunda parte em texto 2
+        texto2 = texto[div:]
+        # inverter a string 
+        texto2 = texto2[::-1]
 
-    # juntar as duas strings
-    print(texto1 + texto2)
+        # juntar as duas strings
+        print(texto1 + texto2)
 
-    #pronto ta funcionando caraca! 
+        #pronto ta funcionando caraca! 
 
