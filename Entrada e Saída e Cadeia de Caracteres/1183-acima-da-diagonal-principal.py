cc524693cd404327320baec95d679693eaa63e26
Abner Santana <abnermuxah@gmail.com>
2020-09-26 13:56:03 -0300

    Create 1183-acima-da-diagonal-principal.py

---
+++ Entrada e Saída e Cadeia de Caracteres/1183-acima-da-diagonal-principal.py
@@ -0,0 +1,32 @@
+op = input()
+matriz=[]
+tam = 12 # matriz é sempre quadrada
+# ler os valores da matriz
+
+for i in range(tam):
+    lista = []
+    for j in range(tam):
+        valor = float(input())
+        lista.append(valor)
+    matriz.append(lista)
+#imprimir a matriz
+#print(matriz)
+# se S entao somar todos elementoss acima da diagonal princial
+if (op == 'S'):
+    total = 0
+    for i in range(tam):
+        for j in range(tam):
+            if not (i>j) and not (i==j):
+                total += matriz[i][j]
+    # exibir a soma total acima da diagonal principal
+    print("{:.1f}".format(total))
+# se M então calcular media acima da diagnonal principal
+elif (op=='M'):
+    total = 0
+    for i in range(tam):
+        for j in range(tam):
+            if not (i>j) and not (i==j):
+                total += matriz[i][j]
+    total = total/(tam*tam)
+    print("{:.1f}".format(total))
+    # mostrar o resultado final
