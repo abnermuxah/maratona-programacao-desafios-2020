8d69151cad55401e55e541ac59de643fe72bb6b5
Abner Santana <abnermuxah@gmail.com>
2020-09-27 18:19:02 -0300

    Update 1828-bazinga!.py

--- Entrada e Saída e Cadeia de Caracteres/1828-bazinga!.py
+++ Entrada e Saída e Cadeia de Caracteres/1828-bazinga!.py
@@ -1,19 +1,15 @@
-T = int(input())
-
-#T = 1
+#T = int(input())
+T = 1
 # entrada (jogada1 (espaço) jogada 2) para cada rodada
-jogadas = []
-for i in range(T):
-    jogadas.append(input())
+
 # processamento e saida (para cada condição) agrupo a lista e avalio cada item
 if (1 <= T <= 100):
+    jogadas = []
     for i in range(T):
-        analise = str(jogadas[i]).split() # dividir a string em 2 e depois analizar
-        print(analise)
-        if (analise[0] == analise[1]):
-            print('Caso #'+ str(i) + ': De Novo!')
-        else:
-            print('Caso #'+ str(i) + ': Raj trapaceou!')
+        jogadas.append(input())
+
+    for i in range(T):
+        analise = str(jogadas[i]).split() # dividir a string i em 2 e depois analizar
         if (analise[0] == 'tesoura' and analise[1] == 'papel'):
             print('Caso #'+ str(i) + ': Bazinga!')
         if (analise[0] == 'papel' and analise[1] == 'pedra'):
@@ -34,6 +30,10 @@ if (1 <= T <= 100):
             print('Caso #'+ str(i) + ': Bazinga!')
         if (analise[0] == 'pedra' and analise[1] == 'tesoura'):
             print('Caso #'+ str(i) + ': Bazinga!')
+        if (analise[0] == analise[1]):
+            print('Caso #'+ str(i) + ': De Novo!')
+        else:
+            print('Caso #'+ str(i) + ': Raj trapaceou!')
         
 
 
