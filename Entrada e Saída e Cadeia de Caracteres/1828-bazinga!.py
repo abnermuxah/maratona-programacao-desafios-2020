e190888ff29c5ebfdd29ad14221a129f61069cf2
Abner Santana <abnermuxah@gmail.com>
2020-09-27 18:35:25 -0300

    Update 1828-bazinga!.py

--- Entrada e Saída e Cadeia de Caracteres/1828-bazinga!.py
+++ Entrada e Saída e Cadeia de Caracteres/1828-bazinga!.py
@@ -1,5 +1,5 @@
-#T = int(input())
-T = 1
+T = int(input())
+#T = 100
 # entrada (jogada1 (espaço) jogada 2) para cada rodada
 
 # processamento e saida (para cada condição) agrupo a lista e avalio cada item
@@ -8,32 +8,32 @@ if (1 <= T <= 100):
     for i in range(T):
         jogadas.append(input())
 
-    for i in range(T):
+    for i in range(len(jogadas)):
         analise = str(jogadas[i]).split() # dividir a string i em 2 e depois analizar
         if (analise[0] == 'tesoura' and analise[1] == 'papel'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'papel' and analise[1] == 'pedra'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'pedra' and analise[1] == 'lagarto'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'lagarto' and analise[1] == 'Spock'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'Spock' and analise[1] == 'tesoura'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'tesoura' and analise[1] == 'lagarto'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'lagarto' and analise[1] == 'papel'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'papel' and analise[1] == 'Spock'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'Spock' and analise[1] == 'pedra'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == 'pedra' and analise[1] == 'tesoura'):
-            print('Caso #'+ str(i) + ': Bazinga!')
-        if (analise[0] == analise[1]):
-            print('Caso #'+ str(i) + ': De Novo!')
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'papel' and analise[1] == 'pedra'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'pedra' and analise[1] == 'lagarto'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'lagarto' and analise[1] == 'Spock'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'Spock' and analise[1] == 'tesoura'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'tesoura' and analise[1] == 'lagarto'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'lagarto' and analise[1] == 'papel'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'papel' and analise[1] == 'Spock'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'Spock' and analise[1] == 'pedra'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == 'pedra' and analise[1] == 'tesoura'):
+            print('Caso #'+ str(i+1) + ': Bazinga!')
+        elif (analise[0] == analise[1]):
+            print('Caso #'+ str(i+1) + ': De Novo!')
         else:
-            print('Caso #'+ str(i) + ': Raj trapaceou!')
+            print('Caso #'+ str(i+1) + ': Raj trapaceou!')
         
 
 
