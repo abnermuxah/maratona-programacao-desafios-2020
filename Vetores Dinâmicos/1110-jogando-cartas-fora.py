252501aa3e14fe741f3508db47db6fd8b46871cc
Abner Santana <abnermuxah@gmail.com>
2020-10-16 18:48:54 -0300

    0

--- Vetores Dinâmicos/1110-jogando-cartas-fora.py
+++ Vetores Dinâmicos/1110-jogando-cartas-fora.py
@@ -1,15 +1,27 @@
 #1110-jogando-cartas-fora
-T = True
-while T == True:
-  cartas = int(input())
-  fim = []
-  num = []
-  if cartas == 0:
-    T = False
-    break
-  for i in range(1, cartas+1):
-    num.append(i)
-  while (len(num) > 1):
-    fim.append(str(num.pop(0)))
-    num.insert(len(num) - 1, num.pop(0))
-  print("Discarded cards: " + ", ".join(fim), "\nRemaining card: " + str(num[0]), end="\n")
+# n cartas (1 até n)
+# cartas 
+
+while True:
+    # ---- Entrada ------ #
+    n = int(input())
+    pilha = []
+    descartadas = []
+    if (n == 0):
+        break
+    # ---- Processamento ------ #
+    for i in range(1,n+1):
+        pilha.append(str(i))
+    # enquanto pilha tiver um elemento (ultimo faça o seguinte)
+    while (len(pilha)!=1):
+        # salve e remova o item do topo da pilha em uma nova lista
+        aux = pilha.pop(0)
+        descartadas.append(str(aux))
+        # coloque o elemento de baixo do topo na base da pilha
+        aux = pilha.pop(0)
+        pilha.append(aux)
+    # ---- Saída ------ #
+    # itens restantes
+    print("Discarded cards:",", ".join(descartadas))
+    # a ultima carta e o unico elemento da pilha
+    print("Remaining card:", ", ".join(pilha))
